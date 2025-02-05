a8f99db98cd42fea626d87ab327b56ac86adaa80
Вадим <pvs.versia@gmail.com>
2025-02-05 02:04:11 +0200

    Next HW-1 with async dynamic routs

--- src/app/users/[userId]/page.tsx
+++ src/app/users/[userId]/page.tsx
@@ -5,10 +5,11 @@ import { IProps } from "./index";
 import css from "./index.module.css";
 
 const UserId: FC<IProps> = async ({ params }) => {
+  const result = await params;
   return (
     <div className={css.main}>
       <ul>
-        {Object.entries(params).map(([key, value]) => (
+        {Object.entries(result).map(([key, value]) => (
           <li key={key}>
             {key}: {value}
           </li>
