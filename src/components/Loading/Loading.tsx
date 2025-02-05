06d308cc3aa14c8c09463ef13750ee92bd01f06d
Вадим <pvs.versia@gmail.com>
2025-02-06 00:02:15 +0200

    Base Config

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { IProps } from "./index";
+
+const Loading: FC<IProps> = () => {
+  return (
+    <div className="flex items-center space-x-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
