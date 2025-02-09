eacdc33f4e2351c323a2b8a1f6d4203c92c21b00
Вадим <pvs.versia@gmail.com>
2025-02-09 12:04:16 +0200

    HW-2 with server action Create

--- src/components/Cards/CarCard/serverActions.ts
+++ src/components/Cards/CarCard/serverActions.ts
@@ -1,7 +1,12 @@
 "use server";
 
 import {apiCarsService} from "@/api/apiCars";
+import {ICar} from "@/common/interfaces/cars.interfaces.ts";
 
 export const fetchDelete = async (id: string) => {
     await apiCarsService.deleteById(id);
+};
+
+export const fetchCreate = async (car: ICar) => {
+    await apiCarsService.create(car);
 };
--- src/components/Forms/CarForm/useCarForm.tsx
+++ src/components/Forms/CarForm/useCarForm.tsx
@@ -3,9 +3,10 @@
 import {Dispatch, SetStateAction, useEffect, useState} from "react";
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {DefaultValues, KeepStateOptions, SubmitHandler} from "react-hook-form";
-import {ICar, ICarCreate} from "@/common/interfaces/cars.interfaces";
+import {ICar} from "@/common/interfaces/cars.interfaces";
 import {useRouter, useSearchParams} from "next/navigation";
 import {apiCarsService} from "@/api/apiCars";
+import {fetchCreate} from "@/components/Cards/CarCard/serverActions.ts";
 
 type IProps = {
     resetAction: (
@@ -33,15 +34,15 @@ export const useCarForm = ({resetAction, item, setCarAction}: IProps) => {
         }
     }, [queryParams, setCarAction]);
 
-    const {mutate: create} = useMutation({
-        mutationFn: (data: ICarCreate) => apiCarsService.create(data),
-        onSuccess: (newCar) => {
-            client.setQueryData<ICar[]>(["cars"], (oldCars = []) => [
-                ...oldCars,
-                newCar,
-            ]);
-        },
-    });
+    // const {mutate: create} = useMutation({
+    //     mutationFn: (data: ICarCreate) => apiCarsService.create(data),
+    //     onSuccess: (newCar) => {
+    //         client.setQueryData<ICar[]>(["cars"], (oldCars = []) => [
+    //             ...oldCars,
+    //             newCar,
+    //         ]);
+    //     },
+    // });
 
     const {mutate: update} = useMutation({
         mutationFn: (data: ICar) => apiCarsService.updateById(data.id.toString(), data),
@@ -61,11 +62,12 @@ export const useCarForm = ({resetAction, item, setCarAction}: IProps) => {
         },
     });
 
-    const onCreate = (data: ICar) => {
+    const onCreate = async (data: ICar) => {
         const newData = {...data, id: Math.floor(Math.random() * 10000)};
         setFormData(newData);
-        create(newData);
+        await fetchCreate(newData);
         resetAction(newData);
+        await client.invalidateQueries({queryKey: ["cars"]})
         router.back();
     };
 
